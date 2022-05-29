84bacd8f93779d9966ce83013db527f6e4fb9cb7
Felipe Rosas <contato@eufelipe.com>
2022-05-29 19:55:53 -0300

    test: add DbSearchNovelByTerm UseCase

--- tests/data/usecases/db-search-novel-by-term.spec.ts
+++ tests/data/usecases/db-search-novel-by-term.spec.ts
@@ -1,6 +1,7 @@
 import faker from '@faker-js/faker'
 import { SearchNovelByTermRepositorySpy } from '@/tests/data/mocks'
 import { DbSearchNovelByTerm } from '@/data/usecases/db-search-novel-by-term'
+import { throwError } from '@/tests/domain/mocks'
 
 type SutTypes = {
     sut: DbSearchNovelByTerm
@@ -29,5 +30,28 @@ describe('DbSearchNovelByTerm UseCase', () => {
         await sut.findByTerm(term)
 
         expect(searchNovelByTermRepositorySpy.term).toBe(term)
+    })
+
+    test('Should throw if SearchNovelByTermRepository throws', async () => {
+        const { sut, searchNovelByTermRepositorySpy } = makeSut()
+        jest.spyOn(
+            searchNovelByTermRepositorySpy,
+            'findByTerm'
+        ).mockImplementationOnce(throwError)
+        const promise = sut.findByTerm(term)
+        await expect(promise).rejects.toThrow()
+    })
+
+    test('Should call SearchNovelByTermRepository if SearchNovelByTermRepository returns []', async () => {
+        const { sut, searchNovelByTermRepositorySpy } = makeSut()
+        searchNovelByTermRepositorySpy.result = []
+        await sut.findByTerm(term)
+        expect(searchNovelByTermRepositorySpy.term).toBe(term)
+    })
+
+    test('Should return Novel Results on success', async () => {
+        const { sut, searchNovelByTermRepositorySpy } = makeSut()
+        const surveyResult = await sut.findByTerm(term)
+        expect(surveyResult).toEqual(searchNovelByTermRepositorySpy.result)
     })
 })
