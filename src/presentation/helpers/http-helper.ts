2e9f4d9d406fdd557046af706d94232058a3aa2d
Felipe Rosas <contato@eufelipe.com>
2022-05-29 18:37:30 -0300

    refactor: add disable no-explicit-any

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,6 +1,7 @@
 import { ServerError } from '@/presentation/errors'
 import { HttpResponse } from '@/presentation/protocols'
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const ok = (data: any): HttpResponse => ({
     statusCode: 200,
     body: data,
--- src/presentation/protocols/http.ts
+++ src/presentation/protocols/http.ts
@@ -1,4 +1,5 @@
 export type HttpResponse = {
     statusCode: number
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     body: any
 }
